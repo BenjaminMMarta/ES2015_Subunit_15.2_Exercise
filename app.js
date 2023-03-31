786770fd393b0608d3e8e575886b4a10cf60bbdb
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-03-31 13:39:31 -0700

    Added ES2015 Subunit 15.2 Exercise in JavaScript file.

---
+++ app.js
@@ -0,0 +1,17 @@
+//--------------------let and const Exercise--------------------//
+
+//ES2015 Global Constants//
+const PI = 3.14;
+
+//Quiz//
+/*1). What is the difference between var and let?
+      You can reassign and redeclare variables. You can't redeclare while using let. Let also creates block scope.
+
+  2). What is the difference between var and const?
+      You can reassign and redeclare variables. You can't redeclare or reassign variables with const. Const also creates block scope.
+
+  3). What is the difference between let and const?
+      You can reassign variables with let, but you can't redeclare or reassign variables with const.
+
+  4). What is hoisting?
+      Variables are moved to the top of the scope they are declared in. With var you can access variable name and it’s undefined value before it's used later on in the code.*/
